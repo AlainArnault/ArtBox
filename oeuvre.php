c9186f460a9f69163351013713148999a611be95
AlainArnault <alain.arnault89@gmail.com>
2024-07-05 19:14:42 +0200

    validation oeuvre.php

--- oeuvre.php
+++ oeuvre.php
@@ -1,3 +1,11 @@
+<?php
+include 'oeuvres.php';
+
+$id = $_GET['id'];
+       
+$detailOeuvre = $oeuvre[$id];
+?>
+
 <!doctype html>
 <html lang="fr">
 <head>
@@ -14,29 +22,19 @@
 
     
 <main>
-<?php include 'oeuvres.php'; ?>
-<?php $_GET['id']; ?>
-<?php $oeuvreId = array_search($_GET['id'], $oeuvre)
-
-
-<?php foreach ($oeuvre as $ficheOeuvre) : ?>
-
     <article id="detail-oeuvre">
         <div id="img-oeuvre">
-            <img src=<?php echo $ficheOeuvre['img-oeuvre']; ?> alt="<?php echo $ficheOeuvre['title']; ?>">
+            <img src=<?php echo $detailOeuvre['img-oeuvre']; ?> alt="<?php echo $detailOeuvre['title']; ?>">
         </div>
         <div id="contenu-oeuvre">
-            <h1>Dodomu</h1>
-            <p class="description">Mia Tozerski</p>
+            <h1><?php echo $detailOeuvre['title']; ?></h1>
+            <p class="description"><?php echo $detailOeuvre['description']; ?></p>
             <p class="description-complete">
-                Mia Tozerski est une artiste peintre ukrainienne réfugiée de la guerre. Sur cette œuvre, Dodomu ("domicile" en ukrainien), elle nous montre la tristesse du peuple ukrainien qu'elle partage, ayant elle-même dû quitter son foyer. L'œuvre évoque le drapeau liquéfié d'une Ukraine en souffrance, pleurant la mort de ses compatriotes. Ce travail chargé d'émotion est le symbole d'un événement qui marquera l'Histoire. Cette peinture à l'acrylique rayonne grâce à son fond lisse et ses mélanges de couleurs éclatantes.
+            <?php echo $detailOeuvre['description-complete']; ?>
             </p>
         </div>
     </article>
-
-<?php endforeach; ?>
-
-    
+ 
 </main>
     
 <?php require_once(__DIR__ . '/footer.php'); ?>
